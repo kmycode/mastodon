5351751c500088d464df14098c0660a8f4fddf5b
KMY <tt@kmycode.net>
2023-09-23 13:31:56 +0900

    Wip: make web backend

--- app/javascript/mastodon/reducers/circles.js
+++ app/javascript/mastodon/reducers/circles.js
@@ -1,4 +1,4 @@
-import { List as ImmutableList, fromJS } from 'immutable';
+import { List as ImmutableList, OrderedSet as ImmutableOrderedSet, fromJS } from 'immutable';
 
 import {
   CIRCLE_FETCH_SUCCESS,
@@ -7,6 +7,12 @@ import {
   CIRCLE_CREATE_SUCCESS,
   CIRCLE_UPDATE_SUCCESS,
   CIRCLE_DELETE_SUCCESS,
+  CIRCLE_STATUSES_FETCH_REQUEST,
+  CIRCLE_STATUSES_FETCH_SUCCESS,
+  CIRCLE_STATUSES_FETCH_FAIL,
+  CIRCLE_STATUSES_EXPAND_REQUEST,
+  CIRCLE_STATUSES_EXPAND_SUCCESS,
+  CIRCLE_STATUSES_EXPAND_FAIL,
 } from '../actions/circles';
 
 const initialState = ImmutableList();
@@ -21,6 +27,31 @@ const normalizeLists = (state, circles) => {
   return state;
 };
 
+const normalizeCircleStatuses = (state, circleId, statuses, next) => {
+  return state.updateIn([circleId, 'statuses'], listMap => listMap.withMutations(map => {
+    map.set('next', next);
+    map.set('loaded', true);
+    map.set('isLoading', false);
+    map.set('items', ImmutableOrderedSet(statuses.map(item => item.id)));
+  }));
+};
+
+const appendToCircleStatuses = (state, circleId, statuses, next) => {
+  return appendToCircleStatusesById(state, circleId, statuses.map(item => item.id), next);
+};
+
+const appendToCircleStatusesById = (state, circleId, statuses, next) => {
+  return state.updateIn([circleId, 'statuses'], listMap => listMap.withMutations(map => {
+    if (typeof next !== 'undefined') {
+      map.set('next', next);
+    }
+    map.set('isLoading', false);
+    if (map.get('items')) {
+      map.set('items', map.get('items').union(statuses));
+    }
+  }));
+};
+
 export default function circles(state = initialState, action) {
   switch(action.type) {
   case CIRCLE_FETCH_SUCCESS:
@@ -32,6 +63,16 @@ export default function circles(state = initialState, action) {
   case CIRCLE_DELETE_SUCCESS:
   case CIRCLE_FETCH_FAIL:
     return state.set(action.id, false);
+  case CIRCLE_STATUSES_FETCH_REQUEST:
+  case CIRCLE_STATUSES_EXPAND_REQUEST:
+    return state.setIn([action.id, 'statuses', 'isLoading'], true);
+  case CIRCLE_STATUSES_FETCH_FAIL:
+  case CIRCLE_STATUSES_EXPAND_FAIL:
+    return state.setIn([action.id, 'statuses', 'isLoading'], false);
+  case CIRCLE_STATUSES_FETCH_SUCCESS:
+    return normalizeCircleStatuses(state, action.id, action.statuses, action.next);
+  case CIRCLE_STATUSES_EXPAND_SUCCESS:
+    return appendToCircleStatuses(state, action.id, action.statuses, action.next);
   default:
     return state;
   }
