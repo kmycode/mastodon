13f4901a60828bfaf33e4a4e44c9edc0c114c00f
KMY <tt@kmycode.net>
2023-10-29 08:09:59 +0900

    スペース消して

--- app/javascript/mastodon/features/ui/index.jsx
+++ app/javascript/mastodon/features/ui/index.jsx
@@ -236,7 +236,7 @@ class SwitchingColumnsArea extends PureComponent {
             <WrappedRoute path='/directory' component={Directory} content={children} />
             <WrappedRoute path={['/explore', '/search']} component={Explore} content={children} />
             <WrappedRoute path={['/publish', '/statuses/new']} component={Compose} content={children} />
-            
+
             <WrappedRoute path={['/@:acct', '/accounts/:id']} exact component={AccountTimeline} content={children} />
             <WrappedRoute path='/@:acct/tagged/:tagged?' exact component={AccountTimeline} content={children} />
             <WrappedRoute path={['/@:acct/with_replies', '/accounts/:id/with_replies']} component={AccountTimeline} content={children} componentParams={{ withReplies: true }} />
