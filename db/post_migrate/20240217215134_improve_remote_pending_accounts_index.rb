119e22b7c9beaa64a6bba0a74caf8f25976d7c9a
KMY <tt@kmycode.net>
2024-02-18 07:02:53 +0900

    Fix test

--- db/post_migrate/20240217215134_improve_remote_pending_accounts_index.rb
+++ db/post_migrate/20240217215134_improve_remote_pending_accounts_index.rb
@@ -6,6 +6,6 @@ class ImproveRemotePendingAccountsIndex < ActiveRecord::Migration[7.1]
   def change
     remove_index :accounts, :remote_pending, unique: false, algorithm: :concurrently
 
-    add_index :accounts, :id, unique: false, algorithm: :concurrently, where: 'domain IS NOT NULL AND remote_pending AND suspended_at IS NOT NULL'
+    add_index :accounts, :id, name: 'index_remote_pending_users', unique: false, algorithm: :concurrently, where: 'domain IS NOT NULL AND remote_pending AND suspended_at IS NOT NULL'
   end
 end
