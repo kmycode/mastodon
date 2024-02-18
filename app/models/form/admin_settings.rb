04350281f27fe2d4b3cc7677aee49270a42b86ce
KMY <tt@kmycode.net>
2024-02-18 10:50:41 +0900

    Add: ローカルからフォローされていない全てのアカウントからのフォローを拒否する設定

--- app/models/form/admin_settings.rb
+++ app/models/form/admin_settings.rb
@@ -65,6 +65,7 @@ class Form::AdminSettings
     enable_local_timeline
     emoji_reaction_disallow_domains
     permit_new_account_domains
+    block_unfollow_account_mention
   ).freeze
 
   INTEGER_KEYS = %i(
@@ -107,6 +108,7 @@ class Form::AdminSettings
     stranger_mention_from_local_ng
     enable_local_timeline
     delete_content_cache_without_reaction
+    block_unfollow_account_mention
   ).freeze
 
   UPLOAD_KEYS = %i(
