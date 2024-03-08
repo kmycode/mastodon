1760ac6bd0db5ec0cf6cbf76e30734229d258968
KMY <tt@kmycode.net>
2024-03-08 10:02:01 +0900

    Change: #532 ElasticSearch設定の外出し

---
+++ app/lib/chewy_config.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'singleton'
+require 'yaml'
+
+class ChewyConfig
+  include Singleton
+
+  def initialize
+    custom_config_file = Rails.root.join('.elasticsearch.yml')
+    default_config_file = Rails.root.join('config', 'elasticsearch.default.yml')
+
+    custom_config = nil
+    custom_config = YAML.load_file(custom_config_file) if File.exist?(custom_config_file)
+    default_config = YAML.load_file(default_config_file)
+
+    @config = default_config.merge(custom_config || {})
+
+    @config = @config.merge(YAML.load_file(Rails.root.join('config', 'elasticsearch.default-ja-sudachi.yml'))) if Rails.env.test?
+  end
+
+  attr_reader :config
+
+  def accounts
+    config['accounts']
+  end
+
+  def accounts_analyzers
+    config['accounts_analyzers']
+  end
+
+  def public_statuses
+    config['public_statuses']
+  end
+
+  def public_statuses_analyzers
+    config['public_statuses_analyzers']
+  end
+
+  def statuses
+    config['statuses']
+  end
+
+  def statuses_analyzers
+    config['statuses_analyzers']
+  end
+
+  def tags
+    config['tags']
+  end
+
+  def tags_analyzers
+    config['tags_analyzers']
+  end
+end
