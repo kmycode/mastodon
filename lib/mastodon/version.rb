12a6af9d9aaaf57ec002e5daf0932421abb13ec2
KMY <tt@kmycode.net>
2024-11-07 08:47:43 +0900

    Fix: バージョン表記の末尾にドットがつく場合がある問題

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -55,12 +55,11 @@ module Mastodon
     def to_s_of_mastodon
       components = [to_a.join('.')]
       components << "-#{prerelease}" if prerelease.present?
-      components << "+#{build_metadata_of_mastodon}" if build_metadata_of_mastodon.present?
       components.join
     end
 
     def build_metadata
-      ['kmyblue', to_s_of_kmyblue, build_metadata_of_mastodon].compact.join('.')
+      ['kmyblue', to_s_of_kmyblue].compact.join('.')
     end
 
     def build_metadata_of_mastodon
@@ -75,6 +74,7 @@ module Mastodon
       components = [to_a.join('.')]
       components << "-#{prerelease}" if prerelease.present?
       components << "+#{build_metadata}" if build_metadata.present?
+      components << "+#{build_metadata_of_mastodon}" if build_metadata_of_mastodon.present?
       components.join
     end
 
