f41165fe8e4976ac21eba38711ed4d912ee3c4ef
KMY <tt@kmycode.net>
2024-12-03 13:48:31 +0900

    Fix lint

--- app/javascript/mastodon/features/list_adder/index.tsx
+++ app/javascript/mastodon/features/list_adder/index.tsx
@@ -52,7 +52,6 @@ const ListItem: React.FC<{
   );
 
   return (
-    // eslint-disable-next-line jsx-a11y/label-has-associated-control
     <label className='lists__item'>
       <div className='lists__item__title'>
         <Icon id='list-ul' icon={ListAltIcon} />
