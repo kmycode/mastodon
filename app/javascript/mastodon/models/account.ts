0c429fb0f1a116911a7cce27fa3b4a09b133292e
Adam Lui <adam@kudoai.com>
2025-05-06 22:57:53 -0700

    Condensed convoluted ternary

--- app/javascript/mastodon/models/account.ts
+++ app/javascript/mastodon/models/account.ts
@@ -178,10 +178,6 @@ export function createAccountFromServerJSON(serverJSON: ApiAccountJSON) {
     ),
     note_emojified: emojify(accountJSON.note, emojiMap),
     note_plain: unescapeHTML(accountJSON.note),
-    url:
-      accountJSON.url.startsWith('http://') ||
-      accountJSON.url.startsWith('https://')
-        ? accountJSON.url
-        : accountJSON.uri,
+    url: accountJSON[/^https?:\/\//.test(accountJSON.url) ? 'url' : 'uri'],
   });
 }
