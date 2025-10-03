7497b5c377dc1299f618cdc401d9f6d275b6fc5f
KMY (Yuki Asuka) <tt@kmycode.net>
2025-10-03 00:20:20 +0000

    Add: ソフトウェア名をアカウントページに表示

--- app/javascript/mastodon/features/account_timeline/components/account_header.tsx
+++ app/javascript/mastodon/features/account_timeline/components/account_header.tsx
@@ -926,6 +926,7 @@ export const AccountHeader: React.FC<{
                 <DomainPill
                   username={username ?? ''}
                   domain={domain ?? ''}
+                  software={account.software}
                   isSelf={me === account.id}
                 />
                 {lockedIcon}
