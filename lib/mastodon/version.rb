88afc089798911e14567e48cf458bd764aa6e3fd
KMY (Yuki Asuka) <tt@kmycode.net>
2026-01-08 08:03:50 +0900

    Bump versiont o 21.5-lts

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -13,7 +13,7 @@ module Mastodon
     end
 
     def kmyblue_minor
-      4
+      5
     end
 
     def kmyblue_flag
@@ -31,7 +31,7 @@ module Mastodon
     end
 
     def patch
-      4
+      3
     end
 
     def default_prerelease
