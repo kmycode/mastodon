2636f2695dbedd2ea571bc1bfbf2087725040e56
KMY (Yuki Asuka) <tt@kmycode.net>
2026-03-25 08:16:00 +0900

    Fix manually merge mistake

--- app/lib/activitypub/activity/create.rb
+++ app/lib/activitypub/activity/create.rb
@@ -301,7 +301,7 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
     return unless @status_parser.quote?
 
     @quote_approval_uri = @status_parser.quote_approval_uri
-    @quote_approval_uri = 'http://kmy.blue/ns#LegacyQuote' if approval_uri == 'kmyblue:LegacyQuote'
+    @quote_approval_uri = 'http://kmy.blue/ns#LegacyQuote' if @quote_approval_uri == 'kmyblue:LegacyQuote'
     @quote_approval_uri = nil if unsupported_uri_scheme?(@quote_approval_uri) || (@quote_approval_uri != 'http://kmy.blue/ns#LegacyQuote' && TagManager.instance.local_url?(@quote_approval_uri))
     @quote = Quote.new(account: @account, approval_uri: nil, legacy: @status_parser.legacy_quote?, state: @status_parser.deleted_quote? ? :deleted : :pending)
   end
