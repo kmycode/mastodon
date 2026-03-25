3facc1d86174efba05ea9a51511be8b781d13de7
KMY (Yuki Asuka) <tt@kmycode.net>
2026-03-25 11:10:39 +0900

    Fix test

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -55,6 +55,10 @@ class Quote < ApplicationRecord
     reset_parent_cache! if attribute_previously_changed?(:state)
   end
 
+  def pend_legacy!
+    update!(approval_uri: 'http://kmy.blue/ns#LegacyQuote')
+  end
+
   def reject!
     if accepted?
       update!(state: :revoked, approval_uri: nil)
