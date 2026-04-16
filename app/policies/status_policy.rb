e6274f8ac4dc3530122d001d4c12d0bb995224b2
KMY (Yuki Asuka) <tt@kmycode.net>
2026-04-16 10:07:27 +0900

    Fix being able to quote someone you blocked (#38608)

--- app/policies/status_policy.rb
+++ app/policies/status_policy.rb
@@ -26,9 +26,8 @@ class StatusPolicy < ApplicationPolicy
     following_author_domain?
   end
 
-  # This is about requesting a quote post, not validating it
   def quote?
-    show? && record.quote_policy_for_account(current_account) != :denied
+    show? && !blocking_author? && record.quote_policy_for_account(current_account, preloaded_relations: @preloaded_relations) != :denied
   end
 
   def reblog?
